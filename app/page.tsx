c259be2e5b32f14d0622c703afdf3f30adef9422
mayankbhatt01 <bhattmanku2005@gmail.com>
2025-09-01 20:26:03 +0530

    Updated Backend Url

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import DashboardPage from "./dashboard/page";
 import LoginPage from "./login/page";
 
 export default function Home() {
-  const token = localStorage.getItem("token");
+  let token:any = "";
+  if (typeof window !== "undefined") {
+    token = localStorage.getItem("token");
+    // use token
+  }
   return (<>
     {token ? <DashboardPage /> :
-      <LoginPage  />}
+      <LoginPage />}
   </>
   );
 }
